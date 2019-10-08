bdc2e1a47635e2d725fc6164c7b72a4270a4573d
yinjiazhang <yinjiazhang7@gmail.com>
2019-10-08 21:00:05 +0300

    jac fig

--- data_process/jac_mul_fig.py
+++ data_process/jac_mul_fig.py
@@ -6,69 +6,71 @@ import time
 import json
 from matplotlib import ticker
 import matplotlib
+
+dataset = sys.argv[1]
 # jaccard similarity category
 keys = [ -1., 0., .02, .04, .06, .08, .1, .12, .14, .16, .18, .2, .4, .6, .8, 1.]
-sigmas = [1., 0.1, 0.01, 0.001, 0.0001]
+sigmas = [0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001]
 
-# jac_stat = [0 for i in xrange(len(keys)-1)]
-# vec_stat = [[0. for i in xrange(len(keys)-1)] for j in xrange(len(sigmas))]
+jac_stat = [0 for i in xrange(len(keys)-1)]
+vec_stat = [[0. for i in xrange(len(keys)-1)] for j in xrange(len(sigmas))]
 
-# print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + " begin loading"
-# for key_index in xrange(len(keys)-1):
-#     count = 0
-#     with open("/data/SDM_result/Jaccard/distance_%f"%keys[key_index+1], 'r') as dist_in:
-#         print '=============================================='
-#         print 'distance_%f'%keys[key_index+1]
-#         line = dist_in.readline()
-#         while line!='':
-#             count += 1
-#             if count%1000000==0:
-#                 print count
-#             line = line.split(',')
-#             jac_stat[key_index] += 1
-#             for line_idx, str_v in enumerate(line):
-#                 if line_idx<=2:
-#                     continue
-#                 vec_stat[line_idx-3][key_index] += float(str_v)
-#             line = dist_in.readline()
-# for i in xrange(len(sigmas)):
-#     for j in xrange(len(keys)-1):
-#         vec_stat[i][j] /= jac_stat[j] if jac_stat[j] > 0 else 1
-# print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + " begin drawing"
+print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + " begin loading"
+for key_index in xrange(len(keys)-1):
+    count = 0
+    with open("/data/SDM_result/Jaccard/%s_distance_%f"%(dataset, keys[key_index+1]), 'r') as dist_in:
+        print '=============================================='
+        print 'distance_%f'%keys[key_index+1]
+        line = dist_in.readline()
+        while line!='':
+            count += 1
+            if count%1000000==0:
+                print count
+            line = line.split(',')
+            jac_stat[key_index] += 1
+            for line_idx, str_v in enumerate(line):
+                if line_idx<=2:
+                    continue
+                vec_stat[line_idx-3][key_index] += float(str_v)
+            line = dist_in.readline()
+for i in xrange(len(sigmas)):
+    for j in xrange(len(keys)-1):
+        vec_stat[i][j] /= jac_stat[j] if jac_stat[j] > 0 else 1
+print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + " begin drawing"
 
-# with open("/data/SDM_result/Jaccard/amazon_distance_stat.json", "w") as dis_out:
-#     out_dict = {"jac_stat":jac_stat, "vec_stat":vec_stat}
-#     print out_dict
-#     json.dump(out_dict, dis_out)
+with open("/data/SDM_result/Jaccard/%s_distance_stat.json"%dataset, "w") as dis_out:
+    out_dict = {"jac_stat":jac_stat, "vec_stat":vec_stat}
+    print out_dict
+    json.dump(out_dict, dis_out)
 
-with open('/data/SDM_result/Jaccard/yelp_distance_stat.json', 'r') as distance_in:
-    dist_json = json.load(distance_in)
-    vec_stat = dist_json['vec_stat']
-    jac_stat = dist_json['jac_stat']
+# with open('/data/SDM_result/Jaccard/yelp_distance_stat.json', 'r') as distance_in:
+#     dist_json = json.load(distance_in)
+#     vec_stat = dist_json['vec_stat']
+#     jac_stat = dist_json['jac_stat']
 
-font = {'family' : 'normal',
-        'weight' : 'normal',
-        'size'   : 13}
+# font = {'family' : 'normal',
+#         'weight' : 'normal',
+#         'size'   : 13}
 
-matplotlib.rc('font', **font)
+# matplotlib.rc('font', **font)
 
-x_keys = [ str(keys[i]) for i in xrange(1, len(keys))]
-#x_labels = [ "=0.00", "(0.00, 0.02]", "(0.02, 0.04]", "(0.04, 0.06]", "(0.06, 0.08]", "(0.08, 0.10]", "(0.10, 0.12]", "(0.12, 0.14]", "(0.14, 0.16]", "(0.16, 0.18]", "(0.18, 0.20]", "(0.20, 0.40]", "(0.40, 0.60]", "(0.60, 0.80]", "(0.80, 1.00]"]
+# x_keys = [ str(keys[i]) for i in xrange(1, len(keys))]
+# #x_labels = [ "=0.00", "(0.00, 0.02]", "(0.02, 0.04]", "(0.04, 0.06]", "(0.06, 0.08]", "(0.08, 0.10]", "(0.10, 0.12]", "(0.12, 0.14]", "(0.14, 0.16]", "(0.16, 0.18]", "(0.18, 0.20]", "(0.20, 0.40]", "(0.40, 0.60]", "(0.60, 0.80]", "(0.80, 1.00]"]
 
-x_labels = [ "0.00", "0.02", "0.04", "0.06", "0.08", "0.10", "0.12", "0.14", "0.16", "0.18", "0.20", "0.40", "0.60", "0.80", "1.00"]
+# x_labels = [ "0.00", "0.02", "0.04", "0.06", "0.08", "0.10", "0.12", "0.14", "0.16", "0.18", "0.20", "0.40", "0.60", "0.80", "1.00"]
 
-non_x_labels = [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",]
-fig = plt.figure(figsize=(15,10))
-ax = plt.subplot()
-ax.bar(x_keys, jac_stat, width=-1, align='edge', color='#000000', edgecolor='#ffffff')
-# for a, b in zip(x_keys, jac_stat):
-#     ax.text(a, b+0.000000001, '%d'%b, ha='right', va= 'bottom',fontsize=11)
-# ax.set_title('Jaccard Similarity Distribution', fontsize = 16)
-# ax.set_xlabel('jaccard similarity')
-# ax.set_ylabel('count')
-ax.set_xticklabels(x_labels, rotation=45, horizontalalignment="center")
-#plt.savefig('JaccardSimilarityDistribution.png')
-plt.show()
+# non_x_labels = [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",]
+# fig = plt.figure(figsize=(15,10))
+# ax = plt.subplot()
+# ax.bar(x_keys, jac_stat, width=-1, align='edge', color='#000000', edgecolor='#ffffff')
+# # for a, b in zip(x_keys, jac_stat):
+# #     ax.text(a, b+0.000000001, '%d'%b, ha='right', va= 'bottom',fontsize=11)
+# # ax.set_title('Jaccard Similarity Distribution', fontsize = 16)
+# # ax.set_xlabel('jaccard similarity')
+# # ax.set_ylabel('count')
+# ax.set_xticklabels(x_labels, rotation=45, horizontalalignment="center")
+# #plt.savefig('JaccardSimilarityDistribution.png')
+# plt.show()
 
 
 # fig = plt.figure()
